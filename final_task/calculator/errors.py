ffcd040345640c5d8b81782dfda6e001f99e7c54
Aleh Slutski <st.alehs@gmail.com>
2019-08-29 23:07:32 +0300

    test errors

--- final_task/calculator/errors.py
+++ final_task/calculator/errors.py
@@ -21,7 +21,7 @@ def check_spaces(exp):
 def bracket(expr):
     count_left = 0
     count_right = 0
-    ex = " ".join(expr).split()
+    ex = " ".join(expr).split(" ")
     for i in ex:
         if i == "(":
             count_left += 1
@@ -34,13 +34,13 @@ def bracket(expr):
 
 def check_digits(expr):
     ex = expr.split()
-    exp = " ".join(expr).split(" ")
     for i in range(len(ex)):
-        if ex[i].isdigit() and ex[i+1].isdigit() and ex[i+2].isdigit():
-            print("ERROR: incorrect input")
-            return True
-        elif exp[i] in operators.keys() and exp[i+1] in operators.keys() \
-                and exp[i+2] in operators.keys():
-            print("ERROR: incorrect input")
-            return True
+        if len(ex) > 1:
+            if ex[i].isdigit() and ex[i+1].isdigit():
+                print("ERROR: incorrect input")
+                return True
+        else:
+            if ex[i] in prec.keys():
+                print("ERROR: incorrect input")
+                return True
     return False
